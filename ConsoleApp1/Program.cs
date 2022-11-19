89cea96999b57330fe8ddd0071ef6bdfc834d1f5
danul <danul@YUMIHASHI>
2022-11-19 16:51:45 +0300

    Nothing

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,25 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello my bot!");
+            string str = "z";
+            string str2 = "Z";
+
+            Console.WriteLine("Here is {0}, again is {1}", str, str2);
+
+            OperatorExamples();
+        }
+        private static void OperatorExamples()
+        {
+            int width = 10;
+            width++;
+
+            int height = 5;
+            int area = height * width;
+
+            string result = "Area";
+            result = result + " is " + area;
+
+            Console.WriteLine(result);
         }
     }
 }
